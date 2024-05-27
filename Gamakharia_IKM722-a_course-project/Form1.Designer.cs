7e9779f1bfa58f17e947254f5a7d5b5e37714b93
Тимур Гамахарія <Tymur.Hamakhariia@infiz.khpi.edu.ua>
2024-05-27 19:33:14 +0300

    Finish Task number1

--- Gamakharia_IKM722-a_course-project/Form1.Designer.cs
+++ Gamakharia_IKM722-a_course-project/Form1.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(411, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(411, 316);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Пуск";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Enabled = false;
+            this.tbInput.Location = new System.Drawing.Point(386, 153);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 23);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 25000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(906, 501);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Button bStart;
+        private TextBox tbInput;
+        private System.Windows.Forms.Timer tClock;
     }
 }
--- Gamakharia_IKM722-a_course-project/Form1.cs
+++ Gamakharia_IKM722-a_course-project/Form1.cs
@@ -2,6 +2,7 @@ namespace Gamakharia_IKM722_a_course_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode; // Режим дозволу / заборони введення даних
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +10,67 @@ namespace Gamakharia_IKM722_a_course_project
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Mode = true;
+        }
 
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tbInput.Enabled = true;// Режим дозволу введення
+                tbInput.Focus();
+                tClock.Start();
+                bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+            }
+            else
+            {
+                tbInput.Enabled = false;// Режим заборонивведення tClock.Stop();
+                bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+            }
+
+
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");// Виведення повідомлення"Минуло 25 секунд" на екран
+             tClock.Start();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tClock.Start();
+                bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+            }
+            else
+            {
+                tClock.Stop();
+                bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+            }
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
