6310625a0d0bfca420f3535ea917915cc93bfce6
Тимур Гамахарія <Tymur.Hamakhariia@infiz.khpi.edu.ua>
2024-05-28 19:52:07 +0300

    Finish Task number 3

--- Gamakharia_IKM722-a_course-project/Form1.cs
+++ Gamakharia_IKM722-a_course-project/Form1.cs
@@ -10,12 +10,15 @@ namespace Gamakharia_IKM722_a_course_project
         }
 
         private void Form1_Load(object sender, EventArgs e)
+           
         {
+            MajorObject = new MajorWork();
+            MajorObject.SetTime();
             About A = new About(); // створення форми About
 
             A.tAbout.Start();
             A.ShowDialog(); // відображення діалогового вікна About
-            MajorObject = new MajorWork();
+            
             this.Mode = true;
         }
 
@@ -81,6 +84,13 @@ namespace Gamakharia_IKM722_a_course_project
                 tClock.Start();
                 e.KeyChar = (char)0;
             }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string s;
+            s = (System.DateTime.Now - MajorObject.GetTime()).ToString();
+            MessageBox.Show(s, "Час роботи програми"); // Виведення часу роботи програми і повідомлення "Час роботи програми" на екран
         }
     }
 }
